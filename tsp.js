a40a47c91faffb5b79fb1bd5c80d7542e9c6e891
dougMR <1236472+dougMR@users.noreply.github.com>
2022-06-28 12:35:46 -0400

    more output text updates

--- tsp.js
+++ tsp.js
@@ -238,9 +238,9 @@ const getShortestOrderWithEndpoints = (points) => {
                 "en-US"
             )}</strong> ways for ${
                 points.length -2 
-            } points (plus static end points).<br/>best distance: ${Math.round(
+            } points (plus static end points)<br/>shortest path: <strong>${Math.round(
                 shortestDist
-            )} (${bestOrder})<br/><span style="font-size: 1.4em; font-weight: bold;">order: ${fullOrder}</span>`;
+            )}</strong> (${bestOrder})<br/><span style="font-size: 1.4em; font-weight: bold;">order: ${fullOrder}</span>`;
             console.log('points: ',points);
             console.log('fullOrder: ',fullOrder);
             const dist = calcTotalDistance(points, fullOrder);
